e9dbc6035aaf0650e10faca96bf640fa95b884fd
darkfrr <darkmatterdevelops@gmail.com>
2022-10-23 12:11:01 +0100

    npcs stuff

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,7 +1,11 @@
+ -- Folder for the NPCs
+ local folder = Instance.new("Folder")
+ folder.Parent =  workspace
+ folder.Name = "NPCs"
  -- Getting the Insert Service for the NPCs.
 
 InsertService = game:GetService("InsertService")
 local ID = 11351588353
 local Model = InsertService:LoadAsset(ID)
-Model.Parent = workspace
+Model.Parent = folder
 Model.Name = " "

5ccaff82e4d00b0d55fb8a014e30f507b765df08
darkfrr <darkmatterdevelops@gmail.com>
2022-10-23 01:28:37 +0100

    submit

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,2 +1,4 @@
-print("Hello world, from server!")
-print("bozo")
+InsertService = game:GetService("InsertService")
+local ID = 11351588353
+local Model = InsertService:LoadAsset(ID)
+Model.Parent = workspace

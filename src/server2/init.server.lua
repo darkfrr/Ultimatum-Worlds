32034f20040f706a76c0222225aafe4e4f0ef6af
darkfrr <darkmatterdevelops@gmail.com>
2022-10-23 14:29:22 +0100

    fdsdf

--- src/server2/init.server.lua
+++ src/server2/init.server.lua
@@ -1,12 +1,3 @@
- -- Folder for the NPCs
- local folder = Instance.new("Folder")
- folder.Parent =  workspace
- folder.Name = "NPCs"
- -- Getting the Insert Service for the NPCs.
-
-InsertService = game:GetService("InsertService")
-local ID = 11351588353
-local Model = InsertService:LoadAsset(ID)
-Model.Parent = folder
-Model.Name = "Dark's NPC"
-local Dark = Model
+local DataStoreService = game:GetService("DataStoreService")
+-- Datastore for Items
+local DataStoreService = game:GetService("DataStoreService")
